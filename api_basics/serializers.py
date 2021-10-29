4ec18c466c878a846fe76eeafc7795392f5fa1ad
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-10-29 16:13:25 +0530

    modal serializer.

--- api_basics/serializers.py
+++ api_basics/serializers.py
@@ -3,21 +3,26 @@ from .models import Article
 
 
 class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    author = serializers.CharField(max_length=255)
-    email = serializers.EmailField(max_length=255)
-    date = serializers.DateTimeField()
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'author']
+        # fields = '__all__'
+
+    # title = serializers.CharField(max_length=255)
+    # author = serializers.CharField(max_length=255)
+    # email = serializers.EmailField(max_length=255)
+    # date = serializers.DateTimeField()
+
+    # # def create(self, validated_data):
+    # #     return self
 
     # def create(self, validated_data):
-    #     return self
+    #     return Article.objects.create(validated_data)
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.author = validated_data.get('author', instance.author)
+    #     instance.email = validated_data.get('email', instance.email)
+    #     instance.date = validated_data.get('date', instance.date)
+    #     instance.save()
+    #     return instance
